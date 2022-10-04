4c22c84ab379b835495379ea719b8755f118b268
douevenfeel <mdxnilovd@gmail.com>
2022-10-04 21:15:59 +0300

    cors settings

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.CLIENT_APP_URL }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, 'static')));
